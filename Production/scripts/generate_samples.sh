d5fb9eb1b261b2136c6f119c25bdc66dade403c0
Laurits7 <laurits.tani@gmail.com>
2021-04-29 16:07:49 +0300

    create ntuples script

--- Production/scripts/generate_samples.sh
+++ Production/scripts/generate_samples.sh
@@ -6,9 +6,7 @@
 # To run the scipt:
 #   bash generate_ntuples.sh $SAMPLE $OUTPUT_DIR $NUM_EVENTS $SEED
 # e.g
-#   bash generate_ntuples.sh TTbar_14TeV_TuneCP5_cfi /afs/cern.ch/work/l/ltani/ 10 1
-#
-# runTheMatrix.py -w upgrade -l 23434 --dryRun
+#   bash generate_samples.sh TTbar_14TeV_TuneCP5_cfi /afs/cern.ch/work/l/ltani/ 10 1
 
 set -e  # If the return code of one command is not 0 and the caller does not check it, the shell script will exit.
 set -x  # prints shell command before executing it
@@ -20,39 +18,38 @@ SEED=$4  # Random seed needed for runTheMatrix.py, each workflow needs to have d
 
 cd $OUTPUT_DIR
 
-# Command for generating configuration file the MC
-cmsDriver.py $SAMPLE \
-  --conditions auto:phase2_realistic_T15 \
-  -n $NUM_EVENTS \
-  --era Run3 \
-  --eventcontent FEVTDEBUGHLT \
-  -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
-  --datatier GEN-SIM \
-  --geometry DB:Extended \
-  --no_exec \
-  --fileout step2_phase1_new.root \
-  --customise Validation/RecoParticleFlow/customize_pfanalysis.customize_step2 \
-  --python_filename=step2_phase1_new.py
+cmsDriver.py DYToLL_M-50_14TeV_pythia8_cff \
+    --conditions auto:phase2_realistic \
+    -n 10 \
+    --era Phase2C8 \
+    --eventcontent FEVTDEBUG \
+    --relval 9000,100 \
+    -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
+    --datatier GEN-SIM \
+    --beamspot HLLHC14TeV \
+    --geometry DB:Extended \
+    --fileout file:step2_phase1_new.root \
+    --no_exec \
+    --customise TauMLTools/Production/customize_pfanalysis.customize_step2 \
+    --python_filename=step2_phase1_new.py
 
-# customize osa defineerida endal python/customize_steps.py failis
 
-#Run the reco sequences
 cmsDriver.py step3 \
-  --conditions auto:phase1_2021_realistic \
-  --era Run3 \
-  -n -1 \
-  --eventcontent FEVTDEBUGHLT \
-  -s RAW2DIGI,L1Reco,RECO,RECOSIM \
-  --datatier GEN-SIM-RECO \
-  --geometry DB:Extended \
-  --no_exec \
-  --filein file:step2_phase1_new.root \
-  --fileout step3_phase1_new.root \
-  --customise Validation/RecoParticleFlow/customize_pfanalysis.customize_step3 \
-  --python_filename=step3_phase1_new.py
+    --conditions auto:phase2_realistic \
+    -n 10 \
+    --era Phase2C8 \
+    --eventcontent FEVTDEBUGHLT,MINIAODSIM,DQM \
+    -s RAW2DIGI,L1Reco,RECO,RECOSIM \
+    --datatier GEN-SIM-RECO,MINIAODSIM,DQMIO \
+    --pileup AVE_200_BX_25ns \
+    --geometry DB:Extended \
+    --customise TauMLTools/Production/customize_pfanalysis.customize_step3\
+    --no_exec \
+    --filein file:step2_phase1_new.root \
+    --fileout file:step3_phase1_new.root \
+    --python_filename=step3_phase1_new.py
 
-
-echo "process.RandomNumberGeneratorService.generator.initialSeed = $SEED" >> step2_phase1_new.py
+echo "process.RandomNumberGeneratorService.generator.initialSeed = 1" >> step2_phase1_new.py
 # cmsRun step2_phase1_new.py
 # cmsRun step3_phase1_new.py
 # cmsRun $PACKAGE_BASE/config/ntuple_config.py
