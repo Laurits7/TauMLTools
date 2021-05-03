e78f194c009dc8a1d20f102916335f670c27c9f5
Laurits7 <laurits.tani@gmail.com>
2021-05-03 10:55:02 +0300

    update generate_samples

--- Production/scripts/generate_samples.sh
+++ Production/scripts/generate_samples.sh
@@ -16,40 +16,41 @@ set -x  # prints shell command before executing it
 # NUM_EVENTS=$3  # Number of events to be analyzed
 # SEED=$4  # Random seed needed for runTheMatrix.py, each workflow needs to have different
 
-cd $OUTPUT_DIR
+SEED=1
 
-cmsDriver.py DYToLL_M-50_14TeV_pythia8_cff \
-    --conditions auto:phase2_realistic \
+cmsDriver.py TTbar_14TeV_TuneCP5_cfi \
+    --conditions auto:phase2_realistic_T15 \
     -n 10 \
-    --era Phase2C8 \
+    --era Phase2C9 \
     --eventcontent FEVTDEBUGHLT \
     --relval 9000,100 \
     -s GEN,SIM,DIGI,L1,DIGI2RAW,HLT \
     --datatier GEN-SIM \
     --beamspot HLLHC14TeV \
-    --geometry DB:Extended \
-    --fileout file:step2_phase1_new.root \
+    --geometry Extended2026D49 \
     --no_exec \
     --customise TauMLTools/Production/customize_pfanalysis.customize_step2 \
-    --python_filename=step2_phase1_new.py
+    --python_filename=step2_phase1_new.py \
+    --fileout step2_phase1_new.root 
 
 
 cmsDriver.py step3 \
-    --conditions auto:phase2_realistic \
+    --conditions auto:phase2_realistic_T15 \
     -n 10 \
-    --era Phase2C8 \
+    --era Phase2C9 \
     --eventcontent FEVTDEBUGHLT \
     -s RAW2DIGI,L1Reco,RECO,RECOSIM \
     --datatier GEN-SIM-RECO \
-    --pileup AVE_200_BX_25ns \
-    --geometry DB:Extended \
-    --customise TauMLTools/Production/customize_pfanalysis.customize_step3\
+    --geometry Extended2026D49 \
     --no_exec \
     --filein file:step2_phase1_new.root \
-    --fileout file:step3_phase1_new.root \
+    --fileout step3_phase1_new.root \
+    --customise TauMLTools/Production/customize_pfanalysis.customize_step3 \
     --python_filename=step3_phase1_new.py
+    # --pileup AVE_200_BX_25ns \
 
-echo "process.RandomNumberGeneratorService.generator.initialSeed = 1" >> step2_phase1_new.py
+
+echo "process.RandomNumberGeneratorService.generator.initialSeed = $SEED" >> step2_phase1_new.py
 cmsRun step2_phase1_new.py
 cmsRun step3_phase1_new.py
 # cmsRun $PACKAGE_BASE/config/ntuple_config.py
